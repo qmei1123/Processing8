11ed3838cf0fdd3b0b120eb18641a62adc7a1cc1
qmei1123 <qmei@bu.edu>
2018-01-30 14:20:42 -0500

    create multiple

--- sketch.js
+++ sketch.js
@@ -9,19 +9,25 @@ function draw() {
         background(0);
 
     //line following mouse
+    line(mouse)
     //custome shape
-    beginShape();
-    noFill();
-    strokeWeight(5);
-    stroke(255);
-    let vertices = map(mouseY, 0, width, 5, 100);
-    for(let a = 0; a < 360; a += vertices) {
-        let x = 100 * sin(a) + 180;
-        let y = 100 * cos(a) + 180;
-        vertex (x, y);
-    }
-    endShape(CLOSE);
-
     
+    for (var x = 0; x <= width; a +=100) {
+      function circles() {
+        beginShape();
+        noFill();
+        strokeWeight(5);
+        stroke(255);
+
+        let vertices = map(mouseY, 0, width, 5, 100);
+
+        for(let a = 0; a < 360; a += vertices) {
+            let x = 100 * sin(a) + 180;
+            let y = 100 * cos(a) + 180;
+            vertex (x, y);
+        }
+        endShape(CLOSE);
+        }  
+    }
     
 }
